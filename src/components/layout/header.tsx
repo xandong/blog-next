54802bf18a08c532f7a7770f0cd63bb7ce7073a8
Alexandre <xandongurgel@gmail.com>
2025-06-13 21:13:16 -0300

    feat: implements base page layout

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image"
 import Link from "next/link"
+
 import { Navbar } from "./navbar"
+import Image from "next/image"
 
 export const Header = async () => {
   return (
     <header className="flex items-center justify-between px-4 py-2">
       <div>
-        <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={48} height={48} />
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo-sm.png" alt="S4Us" width={48} height={48} />
+          <span className="font-bold text-primary text-2xl">Share4us</span>
         </Link>
       </div>
 
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image"
+
+import { getSession } from "@/lib/session"
+
 import {
   Menubar,
   MenubarContent,
@@ -5,35 +9,52 @@ import {
   MenubarMenu,
   MenubarTrigger
 } from "../_ui/menubar"
+import { ToggleTheme } from "../misc/toggle-theme"
 import { LogoutButton } from "./logout-button"
+import { SignIn } from "./sign-in"
 
 export const Navbar = async () => {
-  const user = {
-    user_metadata: {
-      firstName: "John",
-      lastName: "Doe"
-    }
-  }
+  const { user } = await getSession()
 
-  const displayName = `${user?.user_metadata?.firstName} ${user?.user_metadata?.lastName}`
+  const firstNameFirstLetter =
+    user?.name?.split(" ")[0].split("")[0] || "Usuário"
+  const lastNameFirstLetter = user?.name?.split(" ")[1].split("")[0] || ""
+
+  const url =
+    user?.profile_image ||
+    `https://ui-avatars.com/api/?name=${firstNameFirstLetter}%20${lastNameFirstLetter}`
 
   return (
     <nav>
-      <ul className="flex items-center gap-3">
-        <li>{displayName}</li>
-        <Menubar className="bg-transparent border-none focus:bg-transparent p-0 rounded-full">
-          <MenubarMenu>
-            <MenubarTrigger className="bg-transparent p-1 rounded-full">
-              <div className="w-8 h-8 rounded-full bg-neutral-700" />
-            </MenubarTrigger>
+      <ul className="flex items-center gap-2">
+        {user ? (
+          <>
+            <li>{user.name || "Unknown"}</li>
+            <Menubar className="bg-transparent border-none focus:bg-transparent p-0 rounded-full">
+              <MenubarMenu>
+                <MenubarTrigger className="bg-transparent p-1 rounded-full">
+                  <Image
+                    src={url}
+                    alt={user.name || "User Avatar"}
+                    width={32}
+                    height={32}
+                    className="rounded-full"
+                  />
+                </MenubarTrigger>
 
-            <MenubarContent className="mr-4">
-              <MenubarItem>
-                <LogoutButton />
-              </MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
+                <MenubarContent className="mr-4">
+                  <MenubarItem>
+                    <LogoutButton />
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
+          </>
+        ) : (
+          <SignIn />
+        )}
+
+        <ToggleTheme />
       </ul>
     </nav>
   )
