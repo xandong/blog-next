f8005c9c74ba1814e4310b1d1550533a11bc7335
Alexandre <xandongurgel@gmail.com>
2025-06-13 21:15:09 -0300

    chore: allow render all source imagens (only demo)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+        port: "",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+        port: "",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "*", // Demo only
+        port: "",
+        pathname: "/**"
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
